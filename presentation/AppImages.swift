95daea6e4897ebc9f76ed0832b10b7942e2a9239
Daniel <daniel.oliveira@foursys.com.br>
2022-08-29 08:52:19 -0300

    vai

---
+++ presentation/AppImages.swift
@@ -0,0 +1,14 @@
+//
+//  ImagesEnum.swift
+//  presentation
+//
+//  Created by Josicleison on 29/08/22.
+//
+
+import UIKit
+
+struct AppImages {
+    
+    static let chevronDown = UIImage(systemName: "chevron.down")
+    static let chevronUp = UIImage(systemName: "chevron.up")
+}
